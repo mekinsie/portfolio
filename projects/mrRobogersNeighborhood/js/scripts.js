2b690df9f6275b5460ae276cff9e973c6628de3b
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-22 18:05:20 -0800

    Updat JS folder for myRobogersNeighborhood.

--- projects/mrRobogersNeighborhood/js/scripts.js
+++ projects/mrRobogersNeighborhood/js/scripts.js
@@ -2,29 +2,23 @@ $(document).ready(function(){
   $("form.form").submit(function(event){
     event.preventDefault();
     let number =$("input#number").val();
+    let array = [];
     let result = [];
     for (let i=0; i <= number; i ++) {
-      if (i % 100 == 31) {
+      array.push(i.toString());
+      if (array[i].includes("3")) {
         result.push("Won't you be my neighbor? ");
       }
-      else if (i % 100 == 32) {
-        result.push("Won't you be my neighbor? ");
-      }
-      else if (i % 100 == 21) {
+      else if (array[i].includes("2")) {
         result.push("Boop! ");
       }
-      else if (i % 10 == 1) {
+      else if (array[i].includes("1")) {
         result.push("Beep! ");
       }
-      else if (i % 10 == 2) {
-        result.push("Boop! ");
-      }
-      else if (i % 10 == 3) {
-        result.push("Won't you be my neighbor? ")
-      }
       else {
-        result.push(i+ ", ");
+        result.push(array[i]+ ", ");
       }    
+      console.log(array);
     }
     $(".container2").show();
     $("#output").append(result);
